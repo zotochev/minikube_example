98d42110360da00a1e067853b1d7ae9697f73f39
Mikhail Zotochev <mikhail@digitalcube.rs>
2022-10-10 10:14:40 +0200

    works

--- setup.sh
+++ setup.sh
@@ -17,9 +17,6 @@ docker build -t mysql_image srcs/mariadb
 kubectl apply -f srcs/mariadb/srcs/mysql.yaml
 kubectl apply -f srcs/mariadb/srcs/mysql_pvc.yaml
 
-# postgres
-# todo
-
 # nginx
 docker build -t nginx_image srcs/nginx
 kubectl apply -f srcs/nginx/srcs/nginx.yaml
@@ -27,3 +24,10 @@ kubectl apply -f srcs/nginx/srcs/nginx.yaml
 # tornado
 docker build -t image_tornado_0 srcs/tornado_
 kubectl apply -f srcs/tornado_/srcs/tornado_.yaml
+
+# todo
+  # logging
+  # https://www.elastic.co/blog/kubernetes-observability-tutorial-k8s-log-monitoring-and-analysis-elastic-stack
+  # https://www.weave.works/blog/kubernetes-observability-log-aggregation-using-elk-stack
+
+  # postgres
